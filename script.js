3e40935ee689b21979c0f7eaaea287da66fa556a
Tafitasoa <tafitasoa.cedric@gmail.com>
2018-04-05 17:52:55 +0300

    Best google

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let apiv = document.getElementById("apiv");
+let bouton = document.getElementById("bouton");
+
+apiv.style.display = "none";
+bouton.onclick = function(){
+    apiv.style.display = "block";
+    bouton.style.display = "none";
+};
+
